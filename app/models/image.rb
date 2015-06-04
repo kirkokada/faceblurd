3583364af01982c861741e051a4828d3d302d291
Kirk Okada <kirkokada@gmail.com>
2015-06-03 16:10:17 -1000

    Add file size validations

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,7 @@
+require 'file_size_validator'
 class Image < ActiveRecord::Base
   extend FriendlyId
-  validates :file, presence: true
+  validates :file, presence: true, file_size: { maximum: 0.5.megabytes.to_i }
   mount_uploader :file, ImageUploader
   friendly_id :random_string, use: :slugged
 
